2918f91a4b71f6d81c5e8e63ab517545cc2aa449
JangYoonHye <adorej427@naver.com>
2017-08-19 11:54:27 +0900

    Test Commit 2

--- app/src/main/java/com/example/adore/double_j/MainActivity.java
+++ app/src/main/java/com/example/adore/double_j/MainActivity.java
@@ -9,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //장민지 장윤희
     }
 }
