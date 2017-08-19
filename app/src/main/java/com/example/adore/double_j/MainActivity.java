41873351c30e0c2201a1b42856932d875908e824
jangminji <31148563+jangminji@users.noreply.github.com>
2017-08-19 11:46:27 +0900

    Test Commit

--- app/src/main/java/com/example/adore/double_j/MainActivity.java
+++ app/src/main/java/com/example/adore/double_j/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //장민지 장윤희
     }
 }
